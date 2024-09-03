183e73d371a5a0f4421728414ace2b41e1b7db9a
balwan coding <balwansinghrajput1708@gmail.com>
2024-09-03 20:07:19 +0530

    footer add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+    <div className="footer">
+        <p>This is footer section.</p>
+        <ul className="socials">
+            <li><a href="#"><i className="fa-brands fa-facebook"></i></a></li>
+            <li><a href="#"><i className="fa-brands fa-x-twitter"></i></a></li>
+            <li><a href="#"><i className="fa-brands fa-github"></i></a></li>
+            <li><a href="#"><i className="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="#"><i className="fa-brands fa-instagram"></i></a></li>
+        </ul>
+        <div className="footer-copyright">
+            <p>copyright &copy;2022 </p>
+        </div>
+    </div>
+</footer>
+
+  )
+}
