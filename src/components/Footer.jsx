9f8b98ed3ffd8d141632634dfc72f7e1f5287ba1
balwan coding <balwansinghrajput1708@gmail.com>
2024-09-12 18:54:27 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,16 @@
 import React from 'react'
 
-<<<<<<< HEAD
-export default function Footer(props) {
-  return (
-    <footer style={{color: props.mode==='dark'?'white':'#042743'}}>
-    <div className="footer" style={{color: props.mode==='dark'?'white':'#042743'}} >
-=======
 export default function Footer() {
-  return (
-    <footer>
-    <div className="footer">
->>>>>>> 183e73d371a5a0f4421728414ace2b41e1b7db9a
-        <p>This is footer section.</p>
-        <ul className="socials">
-            <li><a href="#"><i className="fa-brands fa-facebook"></i></a></li>
-            <li><a href="#"><i className="fa-brands fa-x-twitter"></i></a></li>
-            <li><a href="#"><i className="fa-brands fa-github"></i></a></li>
-<<<<<<< HEAD
-            <li><a href="https://github.com/balwan-coding"><i className="fa-brands fa-linkedin"></i></a></li>
-            <li><a href="https://www.instagram.com/balwansingh612/"><i className="fa-brands fa-instagram"></i></a></li>
-        </ul>
-        <div className="footer-copyright">
-            <p>copyright &copy;2024 </p>
-=======
-            <li><a href="#"><i className="fa-brands fa-linkedin"></i></a></li>
-            <li><a href="#"><i className="fa-brands fa-instagram"></i></a></li>
-        </ul>
-        <div className="footer-copyright">
-            <p>copyright &copy;2022 </p>
->>>>>>> 183e73d371a5a0f4421728414ace2b41e1b7db9a
+    return (
+        <div className='footer'>
+              <h1>TEXTUTILE</h1>
+              <div className="footer-links">
+              <a href="https://www.facebook.com/profile.php?id=61563499164037" target='_blank'><i className="fa-brands fa-facebook"></i></a>
+              <a href="https://x.com/BalwanSingh08" target='_blank'><i className="fa-brands fa-x-twitter" target='_blank'></i></a>
+              <a href="https://www.linkedin.com/in/balwan-singh-rajput-106335321/" target='_blank'><i className="fa-brands fa-linkedin"></i></a>
+              <a href="https://www.instagram.com/balwansingh612/" target='-blank'><i className="fa-brands fa-instagram"></i></a>
+              </div>
+              <h1>Created by Balvan Singh Rajput</h1>
         </div>
-    </div>
-</footer>
-
-  )
+    )
 }
